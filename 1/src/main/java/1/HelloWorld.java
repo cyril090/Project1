73d2f8dce90754451ec334cb42ed9e90d81238af
cyril090 <cyril.aubertin@gmail.com>
2013-10-20 23:57:31 +0200

    commit1

---
+++ 1/src/main/java/1/HelloWorld.java
@@ -0,0 +1,15 @@
+package 1;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+public class HelloWorld {
+
+	@Inject
+	private Logger logger;
+
+	public void say() {
+		logger.info("Saying hello on console");
+	}
+}
