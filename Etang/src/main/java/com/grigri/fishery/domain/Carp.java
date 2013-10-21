1750ea2ba801c8ab87240288a5dda20f3f0346da
cyril090 <cyril.aubertin@gmail.com>
2013-10-21 22:37:35 +0200

    add weight

--- Etang/src/main/java/com/grigri/fishery/domain/Carp.java
+++ Etang/src/main/java/com/grigri/fishery/domain/Carp.java
@@ -5,10 +5,12 @@ public class Carp {
 
 	private final int age;
 	private final int lengthInCms;
+	private final int weightInKilo;
     
-	public Carp(int age, int lengthInCms) {
+	public Carp(int age, int lengthInCms, int weightInKilo) {
         this.age = age;
         this.lengthInCms = lengthInCms;
+        this.weightInKilo = weightInKilo;
     }
 	
 	public int getAge() {
@@ -18,5 +20,8 @@ public class Carp {
 	public int getLengthInCms() {
         return lengthInCms;
     }
-
+	public int weightInKilo() {
+		return weightInKilo;
+	}
+	
 }
